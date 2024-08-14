9c1a2ace365ea83385d7ae0230bb250a75a31bf4
Nishi Mishra <165827696+Nishi-1805@users.noreply.github.com>
2024-08-14 22:58:05 +0530

    Update app.js

--- routes/app.js
+++ routes/app.js
@@ -4,10 +4,14 @@ const controllers = require('../controllers/apps');
 
 router.get('/', controllers.getAppPage);
 router.get('/currencies', controllers.getCurrencies);
+router.get('/login', controllers.getloginPage);
+router.post('/login',controllers.login);
 router.get('/new-user', controllers.getNewUser);
+
 router.get('/notes', controllers.getNotesPage);
 router.get('/daily', controllers.getDailyExpensePage);
 router.get('/monthly', controllers.getMonthlyExpensePage);
 router.get('/yearly', controllers.getYearlyExpensePage);
+router.post('/users', controllers.createUser);
 
 module.exports = router;
