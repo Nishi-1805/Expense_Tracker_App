9aa3be790e46e9245f85fa589474e51910316a80
Nishi Mishra <165827696+Nishi-1805@users.noreply.github.com>
2024-08-26 21:02:58 +0530

    Update mail.js

--- controllers/mail.js
+++ controllers/mail.js
@@ -2,7 +2,8 @@ const Sib = require('sib-api-v3-sdk', { resolve: '../sib-api-v3-sdk' });
 const dotenv = require('dotenv');
 dotenv.config({ path: './util/.env' });
 const client = Sib.ApiClient.instance
-const apiKey = client.authentications['api-key']
+const apiKey = client.authentications['api-key'];
+
 apiKey.apiKey = process.env.API_KEY
 
 const tranEmailApi = new Sib.TransactionalEmailsApi()
@@ -12,7 +13,7 @@ const sender = {
     name: 'Day-to-Day Expense App'
 }
 
-const sendForgotPasswordEmail = (email, name) => {
+const sendForgotPasswordEmail = (email, name, forgotPasswordRequestId) => {
     const receivers = [
       {
         email,
@@ -21,22 +22,22 @@ const sendForgotPasswordEmail = (email, name) => {
     ];
   
     tranEmailApi.sendTransacEmail({
-        sender,
-        to: receivers,
-        subject: 'Password Reset Link',
-        textContent: `
-            Hello ${name},
-            Please use the following link to reset your password:
-            ${process.env.FRONTEND_URL}/reset-password/${email}
-        `,
+      sender,
+      to: receivers,
+      subject: 'Password Reset Link',
+      textContent: `
+          Hello ${name},
+          Please use the following link to reset your password:
+          http://localhost:3000/reset-password/${forgotPasswordRequestId}
+      `,
     })
     .then((response) => {
-        console.log('Email sent:', response);
+      console.log('Email sent:', response);
     })
     .catch((error) => {
-        console.error('Error sending email:', error);
+      console.error('Error sending email:', error);
     });
-};
-  
+  };
+
   module.exports = { sendForgotPasswordEmail };
   
